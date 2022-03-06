dfa7bb88ec7d68422fcc392725c16ac73c42ee77
Henry Padua <henrypadua@henry.local>
2022-03-06 01:00:14 -0300

    Config Styled Components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,5 @@
-import type { NextPage } from 'next';
+import { Main } from 'components/Main';
 
-const Home: NextPage = () => {
-   return (
-      <div>
-         <h1>Hello World!</h1>
-      </div>
-   );
-};
-
-export default Home;
+export default function Home() {
+   return <Main />;
+}
