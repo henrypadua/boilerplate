b1b44cb6a61222026a07df11a02be8287ff1908b
Henry Padua <henrypadua@henry-2.local>
2022-03-21 09:38:41 -0300

    Config Story book]

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    reactStrictMode: true
 };
+
+const withPWA = require('next-pwa');
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = withPWA({
+   swcMinify: true,
+   experimental: {
+      // Enables the styled-components SWC transform
+      styledComponents: true
+   },
+   pwa: {
+      dest: 'public',
+      disable: !isProd
+   }
+});
 
 module.exports = nextConfig;
